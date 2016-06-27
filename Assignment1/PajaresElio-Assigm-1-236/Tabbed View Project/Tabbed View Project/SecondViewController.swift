70719a59c74652e86b7453bf963a66d2f13cffac
0173 <elio.pajares@gmail.com>
2016-06-27 18:34:42 -0400

    Add Assignment 1

---
+++ Assignment1/PajaresElio-Assigm-1-236/Tabbed View Project/Tabbed View Project/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tabbed View Project
+//
+//  Created by Elio Pajares on 6/25/16.
+//  Copyright © 2016 Elio Pajares. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
