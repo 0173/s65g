6f1a502f1ed5149dbfe92380e4a63e52148db573
0173 <elio.pajares@gmail.com>
2016-07-03 21:28:30 -0400

    create 3UIviewcontrollers subclass
    create 3UIviewcontrollers subclass

--- Assignment2/Assignment2/MainController.swift
+++ Assignment2/Assignment2/MainController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class MainController: UIViewController {
+    
+
+    @IBOutlet weak var buttonToPloblem2: UIButton!
+    
+    @IBAction func buttProb2ActClick(sender: AnyObject) {
+        print("we were clicked")
+    }
+    
+    @IBOutlet weak var buttonToProblem3: UIButton!
+    
+    
+    @IBOutlet weak var buttonToProblem4: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,4 +37,7 @@ class MainController: UIViewController {
 
 
 }
+
+
+
 
